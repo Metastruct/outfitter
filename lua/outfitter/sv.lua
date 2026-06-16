d556ae359eb14bb1bd9470a47fdb8637a300dd39
Python1320 <python1320+gh@gmail.com>
2026-06-16 23:25:48 +0000

    Add dependency test and http test to TestOutfitsOnBots

--- lua/outfitter/sv.lua
+++ lua/outfitter/sv.lua
@@ -112,7 +112,11 @@ function NetData(pl, k, val)
 	end
 
 	if pl.outfitter_skin then
-		SetSkin(pl, pl.outfitter_skin)
+		--TODO: should this be moved?
+		if hook.Run("OutfitterCanSkin", pl) ~= false then
+			dbgn(8,"experimental setskin",pl,pl.outfitter_skin)
+			pl:SetSkin(pl.outfitter_skin)
+		end
 	end
 	PrecacheModel(mdl)
 	-- CyclePlayerModel(pl) -- it needs to happen after networking
@@ -159,6 +163,9 @@ function TestOutfitsOnBots()
 		{ "models/argonian.mdl",                                  646729594 },
 		{ "models/captainbigbutt/vocaloid/apocalypse_miku.mdl",   629121990 },
 		{ "models/player_chibiterasu.mdl",                        503568129 },
+		{ "models/player/dewobedil/persona/yu/default_p.mdl",    1640675931 },
+		{ "models/alvaroports/vrchat/spectrelightpm.mdl",      3731746852, { version = 1, dependencies = { "3486238431" } } },
+		{ "models/pdthcloaker/pdthcloaker.mdl",                   "https://g3cf.metastruct.net/playermodel_test_1.gma" },
 	}
 	for k, v in next, player.GetBots() do
 		local of = t[(k - 1) % (#t) + 1]
