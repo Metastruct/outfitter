1d65e908bb20d16dd03946dad5b0b1c1238ae19d
Python1320 <python1320+gh@gmail.com>
2026-06-16 23:26:20 +0000

    add .outfitter_dependency_manifest to OutfitInfo()

--- lua/outfitter/sh.lua
+++ lua/outfitter/sh.lua
@@ -400,7 +400,8 @@ function Player.OutfitHash(pl)
 end
 
 function Player.OutfitUpdateHash(pl)
-	local mdl, download_path, skin, bodygroups = pl:OutfitInfo()
+	--TODO: can we use manifest from OutfitInfo?
+	local mdl, download_path, skin, bodygroups, _ = pl:OutfitInfo()
 	local hash = GenID(mdl, download_path, skin, bodygroups, pl:OutfitDependencyManifest())
 	pl.outfitter_latest = hash
 
@@ -416,7 +417,7 @@ function Player.OutfitCheckHash(pl, nhash)
 end
 
 function Player.OutfitInfo(pl)
-	return pl.outfitter_mdl, pl.outfitter_download_path, pl.outfitter_skin, pl.outfitter_bodygroups
+	return pl.outfitter_mdl, pl.outfitter_download_path, pl.outfitter_skin, pl.outfitter_bodygroups, pl.outfitter_dependency_manifest
 end
 
 function Player.OutfitSetInfo(pl, mdl, download_path, skin, bodygroups, dependency_manifest)
